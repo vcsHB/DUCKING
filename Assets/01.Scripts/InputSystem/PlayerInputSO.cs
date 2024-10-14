742f1dff6ea891f5bc200abb34df39d5a8afa6b9
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-10-15 00:13:09 +0900

    feat: Input Controls

--- Assets/01.Scripts/InputSystem/PlayerInputSO.cs
+++ Assets/01.Scripts/InputSystem/PlayerInputSO.cs
@@ -1,24 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace InputManage
 {
     
-    [CreateAssetMenu(menuName = "SO/")]    
-    public class PlayerInputSO : ScriptableObject
+    [CreateAssetMenu(menuName = "SO/Input/PlayerInputSO")]    
+    public class PlayerInputSO : ScriptableObject, Controls.IPlayerActions
     {
-        // Start is called before the first frame update
-        void Start()
+        public event Action<Vector2> MovementEvent;
+        public Vector3 AimPosition { get; private set; }
+
+        [SerializeField] private LayerMask _whatIsGround;
+    
+        private Vector3 _lastMousePosition;
+        private Controls _controls;
+
+        private void OnEnable()
         {
-        
+            if(_controls == null)
+            {
+                _controls = new Controls();
+                _controls.Player.SetCallbacks(this);
+                _controls.Player.Enable();
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        public void OnMovement(InputAction.CallbackContext context)
         {
-        
+            MovementEvent?.Invoke(context.ReadValue<Vector2>());
         }
+
+        public void OnAim(InputAction.CallbackContext context)
+        {
+            Vector2 screenPosition = context.ReadValue<Vector2>();
+            AimPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        }
+
     }
 
 }
