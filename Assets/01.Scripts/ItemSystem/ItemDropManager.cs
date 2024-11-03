d4f9309825ed9009796a1bc8fd7d2ce669f0df22
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-11-03 23:40:57 +0900

    fix: Idle Mateiral, DropManager Debug Code

--- Assets/01.Scripts/ItemSystem/ItemDropManager.cs
+++ Assets/01.Scripts/ItemSystem/ItemDropManager.cs
@@ -5,12 +5,22 @@ namespace ItemSystem
 {
     public class ItemDropManager : MonoBehaviour
     {
+        [SerializeField] private ItemInfoGroupSO _itemInfoGroup;
         [SerializeField] private DropItem _dropItemPrefab;
         [SerializeField] private Queue<DropItem> dropItemPool;
 
-        public void GenerateDropItem(int id, int amount)
+
+        [ContextMenu("DebugGenItem")]
+        private void DebugSpawnItem()
         {
-            
+            Vector2 position = new Vector2(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+            GenerateDropItem(0, 1, position);
+        }
+        
+        public void GenerateDropItem(int id, int amount, Vector2 position)
+        {
+            DropItem dropItem = Instantiate(_dropItemPrefab, position, Quaternion.identity);
+            dropItem.Initialize(id, amount, _itemInfoGroup.GetItemData(id));
         }
     }
 }
