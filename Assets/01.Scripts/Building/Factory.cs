a8761c1747077651440c32de3ad6e46b321cc7e6
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-11-13 16:21:16 +0900

    feat: Factroy Structure _1
    임시저장...

--- Assets/01.Scripts/Building/Factory.cs
+++ Assets/01.Scripts/Building/Factory.cs
@@ -1,12 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using ResourceSystem;
 using UnityEngine;
 
-public class Factory : Building
+public class Factory : Source, IResourceInput
 {
-    [SerializeField] private ResourceType[] _canRecieveResourceTypes;
+    [Header("Factory Setting")]
+    [SerializeField] protected SerializeDictionary<ResourceType, int> _requireResources;
+    [SerializeField] protected Resource[] _outputResources;
+    protected Dictionary<ResourceType, int> _storage;
+    [SerializeField] protected int _storageSize;
 
-    
+    //public bool IsEnough => _requireResources.Keys.Where( type => 
 
+    [SerializeField] protected float _processDuration = 2f;
+    private float _currentTime= 0;
+
+    /// <summary>
+    /// 현재 제작 진행상황을 의미 (0~1f 값)
+    /// </summary>
+    public float CurrentProgress => _currentTime / _processDuration;
+
+    public bool TryInsertResource(Resource resource, DirectionEnum inputDir, out Resource remain)
+    {
+        if(!_requireResources.ContainsKey(resource.type))
+        {
+            remain = resource;
+            return false;
+        }
+
+        int over = resource.amount;
+        remain = resource;
+
+        return true;
+    }
 }
