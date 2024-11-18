975e80030464994c0219c950d411f59c5b9fb1da
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-11-19 08:59:03 +0900

    feat: BuildController. Cancel Block Rotation

--- Assets/01.Scripts/Building/BuildingSO.cs
+++ Assets/01.Scripts/Building/BuildingSO.cs
@@ -12,9 +12,10 @@ namespace BuildingManage
 
         public string buildingName;
         public string description;
-        public Sprite buildingIconSprite;        
+        public Sprite buildingIconSprite;   
         public Building building;
         public int tileSize;        //타일맵에서의 사이즈
+        public bool canRotate;
 
         public int health;          //건물의 체력
 
