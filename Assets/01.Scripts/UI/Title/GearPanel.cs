1737db0f13edc8ccd811f5ba977bc19c4acbe249
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-11-21 14:52:10 +0900

    feat: TitleScene UI Design

---
+++ Assets/01.Scripts/UI/Title/GearPanel.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace UI.TitleScene
+{
+
+    public class GearPanel : MonoBehaviour
+    {
+        [SerializeField] private float _spinSpeed;
+
+        private void Update()
+        {
+
+            Rotate();
+        }
+
+        private void Rotate()
+        {
+            float z = transform.rotation.eulerAngles.z + _spinSpeed;
+            transform.rotation = Quaternion.Euler(0, 0, z);
+        }
+    }
+
+}
