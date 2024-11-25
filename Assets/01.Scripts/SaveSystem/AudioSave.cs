d0748aea2331c4da0294389a64efec0f577bd465
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-11-25 16:41:39 +0900

    feat: Audio Save

---
+++ Assets/01.Scripts/SaveSystem/AudioSave.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+namespace SaveSystem
+{
+    public class AudioSave 
+    {
+        public float bgm_volume = 0f;
+        public float sfx_volume = 0f;
+
+    }
+}
--- Assets/01.Scripts/UI/Title/SettingPanel.cs
+++ Assets/01.Scripts/UI/Title/SettingPanel.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using SaveSystem;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -11,33 +13,64 @@ namespace UI.TitleScene
         [SerializeField] private Slider _bgmSlider;
         [SerializeField] private Slider _sfxSlider;
 
+        private string _path;
+
         protected override void Awake()
         {
             base.Awake();
             _bgmSlider.onValueChanged.AddListener(HandleBGMValueChanged);
             _sfxSlider.onValueChanged.AddListener(HandleSFXValueChanged);
+
+            Load();
         }
 
         private void HandleBGMValueChanged(float volume)
         {
             // input=> 0 ~ -40    -40은 -80으로 강제조정
-            if(volume <= -40) volume = -80f;
+            if (volume <= -40) volume = -80f;
 
             _audioMixer.SetFloat("Volume_BGM", volume);
         }
 
         private void HandleSFXValueChanged(float volume)
         {
-            if(volume <= -40) volume = -80f;
+            if (volume <= -40) volume = -80f;
 
             _audioMixer.SetFloat("Volume_SFX", volume);
 
         }
 
-        private void HandleSaveSoundData()
+        public void Save()
         {
-            // 나중에 저장 시스템을 만들어서 
-            
+            AudioSave save = new AudioSave();
+            save.bgm_volume = _bgmSlider.value;
+            save.sfx_volume = _sfxSlider.value;
+
+            string json = JsonUtility.ToJson(save, true);
+            File.WriteAllText(_path, json);
+        }
+
+        public void Load()
+        {
+            _path = Application.dataPath + "/Saves/AudioSetting.json";
+
+            if (!File.Exists(_path))
+            {
+                Save();
+                return;
+            }
+
+            string json = File.ReadAllText(_path);
+            AudioSave save = JsonUtility.FromJson<AudioSave>(json);
+
+            HandleBGMValueChanged(save.bgm_volume);
+            _bgmSlider.value = save.bgm_volume;
+
+            HandleSFXValueChanged(save.bgm_volume);
+            _sfxSlider.value = save.sfx_volume;
+
+
+
         }
     }
 
